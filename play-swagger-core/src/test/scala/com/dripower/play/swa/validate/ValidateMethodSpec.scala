4cd7c8d2f5584cd00c2436875003cf6d3975b00c
KnewHow <948170910@qq.com>
2018-05-25 15:37:48 +0800

    校验器测试完成

--- play-swagger-core/src/test/scala/com/dripower/play/swa/validate/ValidateMethodSpec.scala
+++ play-swagger-core/src/test/scala/com/dripower/play/swa/validate/ValidateMethodSpec.scala
@@ -10,7 +10,16 @@ class ValidateMethodSpec extends FlatSpec {
   "a transfromed trait should call real instance validator method" should "success" in {
     val d  = Demo(1,"123")
     d match {
-      case bv: Demo => println(s"validate result -> ${bv.validator}")
+      case bv: BaseValidate => {
+        val rs = bv.validator
+        if (rs.nonEmpty) {
+          val f = rs(0)
+          println(s"first result ->${f.left.get}")
+        } else {
+          println(s"validate success")
+        }
+
+      }
       case _ => println("match trait failure")
     }
     assert(true)
@@ -20,7 +29,7 @@ class ValidateMethodSpec extends FlatSpec {
 
 case class Demo(id: Int, name: String) extends BaseValidate {
   override def validator = {
-    List(Validator.maxLength(name,4), Validator.biggerThan(id,3)).filter(r => r.isLeft)
+    List(Validator.maxLength(name,2), Validator.biggerThan(id,3)).filter(r => r.isLeft)
   }
 
   def haha = {
